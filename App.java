7253e486b558bfc3bbf890ebc21a23913ed7f0e7
Rahul Bawaskar <rahulbawaskar@gmail.com>
2018-09-19 15:44:47 +0530

    first commit
    first commit

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.TSI.SBS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
